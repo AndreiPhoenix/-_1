4cd873e33fd28bf75badd85408b306ce3ef56629
AndreiPhoenix <145689886+AndreiPhoenix@users.noreply.github.com>
2025-06-30 18:39:15 +0500

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+class Task(Base):
+    __tablename__ = "tasks"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
